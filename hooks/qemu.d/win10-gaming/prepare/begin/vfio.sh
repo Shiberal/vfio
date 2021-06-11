6515494526613e8bb9eb1d6f7e48d444d030f8bb
Francesco <francesco.grelli98@gmail.com>
2021-06-12 01:40:03 +0200

    Update vfio.sh

--- hooks/qemu.d/win10-gaming/prepare/begin/vfio.sh
+++ hooks/qemu.d/win10-gaming/prepare/begin/vfio.sh
@@ -10,10 +10,6 @@ echo 0 > /sys/class/vtconsole/vtcon0/bind
 echo 0 > /sys/class/vtconsole/vtcon1/bind
 sleep 3
 
-modprobe -r amdgpu
-modprobe -r --remove-dependencies drm
-sleep 2
-
 virsh nodedev-detach --device pci_0000_09_00_0
 virsh nodedev-detach --device pci_0000_09_00_1
 sleep 1
